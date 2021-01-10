7b75ea74ac0bfc654a683e76f05673747ea4b558
carol-kangara <carolkangara30@gmail.com>
2021-01-10 15:41:30 +0300

    add the selections

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// //bussiness logic
+// function Order(size,crust,topping){
+//     this.size=size;
+//     this.crust=crust;
+//     this.topping=topping;
+// }
+// //user interface
+// $(document).ready(function(){
+    
+// });
