318f23a72158d38a297ddf92fcac5a76fabcbc83
carol-kangara <carolkangara30@gmail.com>
2021-01-10 16:11:20 +0300

    add js topping price

--- js/script.js
+++ js/script.js
@@ -7,5 +7,49 @@ function Order(size,crust,topping,number){
 }
 //user interface
 $(document).ready(function(){
+    switch (topping) {
+        case topping = "spicy salami":
+            totalPrice = totalPrice + 100;
+            break;
+        case topping = "Caramelised onion":
+            totalPrice = totalPrice + 120;
+            break;
+        case topping = "zucchini":
+            totalPrice = totalPrice + 100;
+            break;
+        case topping = "BBQ chicken":
+            totalPrice = totalPrice + 120;
+            break;
+        case topping = "BBQ Beef":
+            totalPrice = totalPrice + 100;
+            break;
+        case topping = "shredded chicken":
+            totalPrice = totalPrice + 120;
+            break;
+        case topping = "red onins":
+            totalPrice = totalPrice + 100;
+            break;
+        case topping = "smoked paprika":
+            totalPrice = totalPrice + 120;
+            break;
+        case topping = "spinach":
+            totalPrice = totalPrice + 120;
+            break;
+        case topping = "capsicum":
+            totalPrice = totalPrice + 150;
+            break;        
+        case topping = "sliced jalapenos":
+            totalPrice = totalPrice + 150;
+            break;
+        case topping = "hot sauce":
+            totalPrice = totalPrice + 120;
+            break;
+        case topping = "beef mince":
+            totalPrice = totalPrice + 120;
+            break;
+    }
+    
+        
+
     
 });
