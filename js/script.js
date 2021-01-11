5346d9e7849a163d2db1bd9a04283059ae2d4cb0
carol-kangara <carolkangara30@gmail.com>
2021-01-11 22:11:38 +0300

    add js variables

--- js/script.js
+++ js/script.js
@@ -14,15 +14,15 @@ this.delivery=delivery;
     
    
 // };
-// $(document).ready(function(){
-//     let deliveryPrice=parseInt(250);
-//     let toppingPrice=parseInt(200);
-//     let inputtedSize =$("#size option:selected").val();
-//     let inputtedCrust=$("#crust option:selected").val();
-//     let inputtedTopping1=$("#topping1 option:selected").val();
-//     let inputtedTopping2=$("#topping2 option:selected").val();
-//     let inputtedNumber=$("#number").val();
-//     let newOrder = new Order( inputtedSize ,inputtedCrust,inputtedTopping1,inputtedTopping2,inputtedNumber);
+$(document).ready(function(){
+    let deliveryPrice=parseInt(250);
+    let toppingPrice=parseInt(200);
+    let inputtedSize =$("#size option:selected").val();
+    let inputtedCrust=$("#crust option:selected").val();
+    let inputtedTopping1=$("#topping1 option:selected").val();
+    let inputtedTopping2=$("#topping2 option:selected").val();
+    let inputtedNumber=$("#number").val();
+    let newOrder = new Order( inputtedSize ,inputtedCrust,inputtedTopping1,inputtedTopping2,inputtedNumber);
      
 //     $("#orders").click(function(){
 //         if(inputtedNumber=1){
