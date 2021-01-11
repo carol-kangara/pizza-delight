3fcf38e841064f2f62c5ca53d782d4d8f23f2eb2
carol-kangara <carolkangara30@gmail.com>
2021-01-11 21:59:13 +0300

    add selectin form

--- js/script.js
+++ js/script.js
@@ -1,108 +1,36 @@
-//bussiness logic
-function Order(size,crust,topping,number){
-    this.size=size;
-    this.crust=crust;
-    this.topping=topping;
-    this.number=number;
-}
-//user interface
-$(document).ready(function(){
-    switch (size) {
-        case size = "regular":
-            if (crust === "cripsy") {
-                totalPrice = (300 * number);
-            } else if (crust === "Glutten-free") {
-                totalPrice = (250 * number);
-            } else if (crust === "stuffed") {
-                totalPrice = (300 * number);
-            } else if (crust === "sicilian style") {
-                totalPrice = (250 * number);
-            } else if (crust === "Chicago-style deep dish") {
-                totalPrice = (250 * number);
-            } else if (crust === "New york style") {
-                totalPrice = (350 * number);
-            }
-            else {
-                totalPrice = (350 * number);
-            }
-            break;
-        case size = "medium":
-            if (crust === "cripsy") {
-                totalPrice = (600 * number);
-            } else if (crust === "Glutten-free") {
-                totalPrice = (5500 * number);
-            } else if (crust === "stuffed") {
-                totalPrice = (600 * number);
-            } else if (crust === "sicilian style") {
-                totalPrice = (550 * number);
-            } else if (crust === "Chicago-style deep dish") {
-                totalPrice = (550 * number);
-            } else if (crust === "New york style") {
-                totalPrice = (650 * number);
-            }
-            else {
-                totalPrice = (750 * number);
-            }
-            break;
-        case size = "large":
-            if (crust === "cripsy") {
-                totalPrice = (1100 * number);
-            } else if (crust === "Glutten-free") {
-                totalPrice = (1000 * number);
-            } else if (crust === "stuffed") {
-                totalPrice = (1100 * number);
-            } else if (crust === "sicilian style") {
-                totalPrice = (1000 * number);
-            } else if (crust === "Chicago-style deep dish") {
-                totalPrice = (1000 * number);
-            } else if (crust === "New york style") {
-                totalPrice = (1150 * number);
-            }
-            else {
-                totalPrice = (1200 * number);
-            }
-            break;
-    }
-    switch (topping) {
-        case topping = "spicy salami":
-            bill= totalPrice + 100;
-            break;
-        case topping = "Caramelised onion":
-            bill = totalPrice + 120;
-            break;
-        case topping = "zucchini":
-            bill = totalPrice + 100;
-            break;
-        case topping = "BBQ chicken":
-            bill = totalPrice + 120;
-            break;
-        case topping = "BBQ Beef":
-            bill = totalPrice + 100;
-            break;
-        case topping = "shredded chicken":
-            bill = totalPrice + 120;
-            break;
-        case topping = "red onins":
-            bill = totalPrice + 100;
-            break;
-        case topping = "smoked paprika":
-            bill = totalPrice + 120;
-            break;
-        case topping = "spinach":
-            bill = totalPrice + 120;
-            break;
-        case topping = "capsicum":
-            bill = totalPrice + 150;
-            break;        
-        case topping = "sliced jalapenos":
-            bill = totalPrice + 150;
-            break;
-        case topping = "hot sauce":
-            bill = totalPrice + 120;
-            break;
-        case topping = "beef mince":
-            bill = totalPrice + 120;
-            break;
-    }
-        
-});
+// function Order(size,crust,topping1,topping2,number,delivery){
+// this.size=size;
+// this.crust=crust;
+// this.topping1=topping1;
+// this.topping2=topping2;
+// this.number=number;
+// this.delivery=delivery;
+// }
+// function Final (crustPrice,toppingsPrice , deliveryPrice ) {;
+//     this.crustPrice=crustPrice;
+//     this.toppingsPrice=toppingsPrice;
+//     this.deliveryPrice=deliveryPrice;
+//     this.number=number;
+    
+   
+// };
+// $(document).ready(function(){
+//     let deliveryPrice=parseInt(250);
+//     let toppingPrice=parseInt(200);
+//     let inputtedSize =$("#size option:selected").val();
+//     let inputtedCrust=$("#crust option:selected").val();
+//     let inputtedTopping1=$("#topping1 option:selected").val();
+//     let inputtedTopping2=$("#topping2 option:selected").val();
+//     let inputtedNumber=$("#number").val();
+//     let newOrder = new Order( inputtedSize ,inputtedCrust,inputtedTopping1,inputtedTopping2,inputtedNumber);
+     
+//     $("#orders").click(function(){
+//         if(inputtedNumber=1){
+//         alert("your have ordered a: " + inputtedSize  + ' ' + inputtedCrust + ' ' + inputtedTopping + ' ' +inputtedNumber + "pizza!");
+//         alert("The total cost is" +sizePrice.Order+ '' +crustPrice.Order + ''+ deliveryPrice  + ''+ toppingPrice + ".");
+//         }
+//     })
+
+    
+
+// });
