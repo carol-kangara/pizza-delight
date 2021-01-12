40c49ac24bb7f6091cbbddae5eb3f43666313ee6
carol-kangara <carolkangara30@gmail.com>
2021-01-12 08:17:10 +0300

    add README

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # pizza-delight
+####  
+#### By CAROLINE KANG'ARA
+## Description
+This is a project about  a pizza online shop ,you make your desired choice and then its is delivered if you want.
+my github repository link is https://github.com/carol-kangara/pizza-delight
+The website link: https://carol-kangara.github.io/delani-studio/
+## Setup/Installation Requirements 
+* Click on my github  repository link
+* Fork the repository
+* once you get the copy in your github,you can open the vs code and enter the clone code
+*You  should the download bootstrap version 4.5 and above
+* You need to install vscode or atom or text editor
+## Known Bugs
+Malichimp API is not working correctly but i am working on it.
+## Technologies Used
+*css
+*Html
+*JavaScript
+*jQuery
+* Bootstrap
+## Support and contact details
+you can reach me using the following:
+* phone:0101234568
+* email:me@mail.com
--- js/script.js
+++ js/script.js
@@ -1,36 +1,65 @@
-function Order(size,crust,topping1,topping2,number,delivery){
+let deliveryPrice=parseInt(250);
+function Order(size,crust,){
 this.size=size;
 this.crust=crust;
-this.topping1=topping1;
-this.topping2=topping2;
-this.number=number;
-this.delivery=delivery;
+this.topping1=200;
+this.topping2=250;
+this.number=1;
+this.numbers=[];
 }
-// function Final (crustPrice,toppingsPrice , deliveryPrice ) {;
-//     this.crustPrice=crustPrice;
-//     this.toppingsPrice=toppingsPrice;
-//     this.deliveryPrice=deliveryPrice;
-//     this.number=number;
-    
-   
-// };
+Order.prototype.myOrder=function(){
+   if(this.Size==regular){
+       this.sizePrice +=300;
+   } else if(this.size==medium){
+       this.sizePrice +=600
+   }else if(this.size==large){
+       this.sizePrice +=1100;
+   }
+   if(this.crust==crispy){
+       this.sizeprice +=200;  
+   }
+   else if(this.crust==Glutten-free){
+       this.sizePrice +=250
+   }
+   else if(this.crust==stuffed){
+       this.sizePrice +=200
+   }
+   else if(this.crust==sicilian-style){
+    this.sizePrice +=300
+   }
+   else if(this.crust==chicago-style){
+    this.sizePrice +=300
+   }
+   else if(this.crust==New-york-style){
+    this.sizePrice +=250
+   }
+   else if(this.crust==custom-crust){
+    this.sizePrice +=200
+   }
+   for(var i= 1; i < this.Numbers.length; i++){
+    pizzaNumber += pizzaNumbers[i];
+      }
+}
+
 $(document).ready(function(){
-    let deliveryPrice=parseInt(250);
-    let toppingPrice=parseInt(200);
     let inputtedSize =$("#size option:selected").val();
     let inputtedCrust=$("#crust option:selected").val();
     let inputtedTopping1=$("#topping1 option:selected").val();
     let inputtedTopping2=$("#topping2 option:selected").val();
-    let inputtedNumber=$("#number").val();
-    let newOrder = new Order( inputtedSize ,inputtedCrust,inputtedTopping1,inputtedTopping2,inputtedNumber);
-     
-    $("#orders").click(function(){
-        if(inputtedNumber=1){
-        alert("your have ordered a: " + inputtedSize  + ' ' + inputtedCrust + ' ' + inputtedTopping + ' ' +inputtedNumber + "pizza!");
-        alert("The total cost is" +sizePrice.Order+ '' +crustPrice.Order + ''+ deliveryPrice  + ''+ toppingPrice + ".");
-        }
-    })
+    let inputtedNumber=$("#number").val();   
+    $("#orders").click(function(event){
+        event.preventDefault()
 
-    
+        if(inputtedNumber==1 || inputtedNumber<=1 ){
+        alert("your have ordered a: " + inputtedNumber + '' + inputtedSize  + ' ' + inputtedCrust + ' ' + inputtedTopping1 + ' ' + inputtedTopping2  + "pizza!");
+        }
+        alert("your will pay: " + sizePrice +".");
+        }
+    });
+
+    $("ul#order").append("<li>Your order is " + newOrder.Order + "</li>")
 
 });
+function buttonClick(){
+    alert ("Thankyou for shopping with us")
+};
